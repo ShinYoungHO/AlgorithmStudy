6c80146ab502c40eccc2789872a340ca04ccce3d
ShinYoungHO <dydh1230219@gmail.com>
2021-02-12 01:38:28 +0900

    2016day

--- algorithmExamples/programmers/스킬트리.js
+++ algorithmExamples/programmers/스킬트리.js
@@ -21,7 +21,7 @@
 // BACDE: B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가능한 스킬트립니다.
 // CBADF: 가능한 스킬트리입니다.
 // AECB: 가능한 스킬트리입니다.
-// BDA: B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가능한 스킬트리입니다.
+// BDA: B 스킬을 배우기 전에 C 스킬을 먼저 배워야 합니다. 불가능한 스킬트리입니다. 
 function solution(skill, skill_trees) {
     let tempArr = skill.split("");
     let tempStr = 0;
