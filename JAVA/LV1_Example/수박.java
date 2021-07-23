fa6798c3077fa2d42918b17f1749358a30240dfa
shinyoungho <youngho@shinyounghoui-MacBookAir.local>
2021-07-23 13:45:09 +0900

    JAVA StringBuffer

---
+++ JAVA/LV1_Example/수박.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public String solution(int n){
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < n; i++){
+            sb.append(i % 2 == 0 ? "수" : "박");
+        }
+        return sb.toString();
+    }
+}
